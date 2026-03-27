b65626f1ce581209c218e0c72c61d76fae3f9317
Azaria Yonas <azariayonas8@gmail.com>
2026-03-27 02:21:37 -0700

    Initial Commit

--- Backend/routes/tasks.py
+++ Backend/routes/tasks.py
@@ -1,11 +1,27 @@
-from flask import Flask
-import psycopg as pg
-import supabase
+from clients.psycopg_connect import psycopg_connect
 
 
 
 
-def get_tasks():
+def fecth_tasks(user_id):
+    results = []
+    with psycopg_connect() as conn:
+        with conn.cursor() as curr:
+            curr.execute("""
+                SELECT title, description, due_date, color FROM mydb.tasks
+                WHERE user_id = %s
+                ORDER BY order_index
+            """,
+            (user_id,)) 
 
+            rows = curr.fetchall()
 
-    
+            for row in rows:
+                results.append({
+                    "title": row[0],
+                    "description": row[1],
+                    "due_date": row[2],
+                    "color": row[3],
+                })
+
+    return results
