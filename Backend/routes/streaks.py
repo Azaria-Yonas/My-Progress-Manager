e740700734b20fb274dcaeaf40e37f26d147ee72
Azaria Yonas <azariayonas8@gmail.com>
2026-03-27 02:21:16 -0700

    Initial Commit

--- Backend/routes/streaks.py
+++ Backend/routes/streaks.py
@@ -1,8 +1,26 @@
-from flask import Flask
-import psycopg as pg
-import supabase
+from clients.psycopg_connect import psycopg_connect
 
 
 
 
-def fecth_streaks
+def fecth_streaks(user_id):
+    results = []
+    with psycopg_connect() as conn:
+        with conn.cursor() as curr:
+            curr.execute("""
+                SELECT title, start_time, interval_seconds, streak_count FROM mydb.streaks
+                WHERE user_id = %s
+            """,
+            (user_id,))
+
+            rows = curr.fetchall()
+
+            for row in rows:
+                results.append({
+                    "title": row[0],
+                    "start_time": row[1],
+                    "interval_seconds": row[2],
+                    "streak_count": row[3],
+                })
+
+    return results
