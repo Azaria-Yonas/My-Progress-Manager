580abe97dc74f913e821212f2ecdf62fb823066a
Azaria Yonas <azariayonas8@gmail.com>
2026-04-01 16:05:37 -0700

    Added tasks and streaks to route table + Verifying User ID

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,10 @@
+# app.py
 from flask import Flask, request
-from clients.supabase_client import new_client      # Not Needed
-from auth.login import login
-from auth.signup import signup
-
-
-
+from auth.auth import authenticate_userid
+from routes.login import login
+from routes.signup import signup
+from routes.tasks import fecth_tasks
+from routes.streaks import fecth_streaks
 
 
 
@@ -16,13 +16,26 @@ app = Flask(__name__)
 def login_route():
     return login()
 
-
 @app.route("/sigup", methods=["POST"])              # SignUp
 def signup_route_route():
     return signup()
 
 
+@app.route("/tasks", methods = ["GET"])             # Fetch Tasks
+def ftasks():
+    try:
+        id = authenticate_userid(request)
+        return fecth_tasks(id)
+    except Exception as e:
+        return str(e), 400
 
+@app.route("/streaks", methods = ["GET"])           # Fetch Streaks
+def fstreaks():
+    try:
+        id = authenticate_userid(request)
+        return fecth_streaks(id)
+    except Exception as e:
+        return str(e), 400
 
 
 
@@ -34,5 +47,5 @@ def home():
     return "Backend is running"
 
 
-if "__main__" == __name__:
+if __name__ == "__main__":
     app.run(debug=True)
