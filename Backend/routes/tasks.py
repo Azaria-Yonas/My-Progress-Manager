36c6f542bd145ba1707ee9f0a6a731f132264ce0
Azaria Yonas <azariayonas8@gmail.com>
2026-04-19 21:23:19 -0700

    Created a new route for creating tasks

--- Backend/routes/tasks.py
+++ Backend/routes/tasks.py
@@ -29,3 +29,32 @@ def fecth_tasks(user_id):
                 })
 
     return jsonify(results)
+
+
+def create_task(user_id, title, description, color, due_date):
+    try:
+        with psycopg_connect() as conn:
+            with conn.cursor() as curr:
+                curr.execute("""
+                    INSERT INTO mydb.tasks (user_id, title, description, color, due_date)
+                    VALUES (%s, %s, %s, %s, %s)
+                    RETURNING id
+                """,
+                (user_id, title, description, color, due_date))
+
+                row = curr.fetchone()
+                if row is None:
+                    raise Exception("Insert failed: no ID returned")
+
+                task_id = row[0]
+            
+                return jsonify({
+                    "id": task_id,
+                    "title": title,
+                    "description": description,
+                    "color": color,
+                    "due_date": due_date,
+                    "is_completed": False
+                })
+    except Exception as e:
+        return jsonify({"Error: ": str(e)}), 400
