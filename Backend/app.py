d2a439cc9558035d1f0ab74d62f1daf5ffd3123c
Azaria Yonas <azariayonas8@gmail.com>
2026-04-19 17:26:13 -0700

    Created new routes for Creating Tasks and Updating Tasks

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,9 @@
 # app.py
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from auth.auth import authenticate_userid
 from routes.login import login
 from routes.signup import signup
-from routes.tasks import fecth_tasks
+from routes.tasks import fecth_tasks, create_task, update_task, delete_task, complete_task
 from routes.streaks import fecth_streaks
 
 
@@ -28,6 +28,29 @@ def ftasks():
         return fecth_tasks(id)
     except Exception as e:
         return str(e), 400
+    
+@app.route("/tasks", methods = ["POST"])            # Create Tasks
+def ctasks():
+    try:
+        data = request.json
+        title = data["title"]
+        description = data["description"]
+        color = data["color"]
+        created_at = data["created_at"]
+        due_date = data["due_date"]
+        id = authenticate_userid(request)
+        return create_task(id, title, description, color, created_at, due_date)
+    except Exception as e:
+        return str(e), 400
+
+@app.route("/task", methods=["PUT"])
+def utasks():
+    return jsonify({"Not Yet Implemented"})
+ 
+
+
+
+
 
 @app.route("/streaks", methods = ["GET"])           # Fetch Streaks
 def fstreaks():
