1d4462789a02583ce68a07e7396179441690b380
Azaria Yonas <azariayonas8@gmail.com>
2026-05-19 02:39:43 -0700

    SQL Query Error Addressed

--- Backend/routes/tasks.py
+++ Backend/routes/tasks.py
@@ -11,6 +11,7 @@ def fecth_tasks(user_id):
             curr.execute("""
                 SELECT id, title, is_completed, color, due_date, order_index 
                 FROM public.tasks
+                WHERE id = %s
             """,
             (user_id,)) 
 
