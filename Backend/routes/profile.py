cb4e3d1bc5de3342214e5fa3409eadcb22edac03
Azaria Yonas <azariayonas8@gmail.com>
2026-05-23 22:31:48 -0700

    Implemented Route For user Meta data retrival and Endpoint for signout

---
+++ Backend/routes/profile.py
@@ -0,0 +1,27 @@
+from flask import jsonify
+import psycopg as pg
+from clients.psycopg_connect import psycopg_connect
+from clients.supabase_client import new_client
+
+
+def getme(id):
+    try:
+        with psycopg_connect() as conn:
+            with conn.cursor() as curr:
+                curr.execute("""
+                    SELECT created_at, raw_user_meta_data FROM auth.users 
+                    WHERE id = %s 
+                """,
+                (id,))
+        return jsonify({"message: ": "Successful Retrived User Meta Data"})
+    except pg.Error as e:
+        return jsonify({"Error: " : str(e)}), 400
+
+
+
+def signout(id):
+    try:
+        new_client().auth.sign_out(id)
+        return jsonify({"message: ": "Successful Signed Out"})       
+    except Exception as e:
+        return jsonify({"Error: ": str(e)}), 400
