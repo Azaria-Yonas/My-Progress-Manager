34447a9d3941e7f5c1dbfe4db4032783f1359a08
Azaria Yonas <azariayonas8@gmail.com>
2026-05-30 00:00:33 -0700

    Creating Routes for retrieving compeleted tasks

---
+++ Backend/routes/stats.py
@@ -0,0 +1,32 @@
+from flask import jsonify
+import psycopg as pg
+from clients.psycopg_connect import psycopg_connect
+
+
+def tasks_analytics(id):
+    results = []
+    try:
+        with psycopg_connect() as conn:
+            with conn.cursor() as curr:
+                curr.execute("""
+                    SELECT id, title, color, due_date, completed_at 
+                    WHERE user_id = %s
+                """,
+                (id,))
+
+                
+                rows = curr.fetchall()
+
+                for row in rows:
+                    results.append({
+                        "id": row[0], 
+                        "title": row[1], 
+                        "color": row[2], 
+                        "due_date": row[4], 
+                        "completed_at": row[5]
+                    })
+    except pg.Error as e:
+        return jsonify({"Error: ": str(e)})
+        
+
+
