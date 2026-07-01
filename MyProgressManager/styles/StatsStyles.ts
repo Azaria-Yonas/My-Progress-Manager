3be33f54dc6ab01ee2ade194197ef119c04b3b0a
Azaria Yonas <azariayonas8@gmail.com>
2026-06-30 19:33:19 -0700

    Polished style sheets

--- MyProgressManager/styles/StatsStyles.ts
+++ MyProgressManager/styles/StatsStyles.ts
@@ -1,17 +1,18 @@
 // styles/StatsStyles.ts
 import { StyleSheet } from "react-native";
+import { scale, verticalScale, fontScale } from "../utils/responsive";
 
 export const createStatsStyles = (theme: any) =>
   StyleSheet.create({
     container: {
       flex: 1,
-      paddingTop: 60,
+      paddingTop: verticalScale(60),
     },
 
     scrollContent: {
-      paddingHorizontal: 20,
-      paddingBottom: 80,
-      paddingTop: 130,
+      paddingHorizontal: scale(20),
+      paddingBottom: verticalScale(80),
+      paddingTop: verticalScale(130),
     },
 
     center: {
@@ -21,49 +22,47 @@ export const createStatsStyles = (theme: any) =>
       backgroundColor: theme.background,
     },
 
-
     card: {
-      padding: 20,
-      borderRadius: 16,
-      marginTop: 10,
-      marginBottom: 20,
+      padding: scale(20),
+      borderRadius: scale(16),
+      marginTop: verticalScale(10),
+      marginBottom: verticalScale(20),
       backgroundColor: theme.card,
+      borderWidth: StyleSheet.hairlineWidth,
+      borderColor: theme.border,
       shadowColor: theme.shadowcolor,
-      shadowOpacity: 0.06,
-      shadowRadius: 5,
-      shadowOffset: { width: 0, height: 2 },
-      elevation: 6,
+      shadowOpacity: 0.08,
+      shadowRadius: scale(6),
+      shadowOffset: { width: 0, height: verticalScale(2) },
+      elevation: 4,
     },
 
     sectionTitle: {
-      marginBottom: 12,
+      marginBottom: verticalScale(12),
     },
 
-
     emptyTextSmall: {
-      marginTop: 8,
-      color: theme.text,
-      opacity: 0.7,
-      fontSize: 13,
+      marginTop: verticalScale(8),
+      color: theme.textMuted,
+      fontSize: fontScale(13),
       textAlign: "center",
     },
 
-
     gridContainer: {
-      marginTop: 16,
+      marginTop: verticalScale(16),
     },
 
     gridRow: {
       flexDirection: "row",
       alignItems: "center",
-      marginBottom: 6,
+      marginBottom: verticalScale(6),
     },
 
     intervalTile: {
-      width: 32,
-      height: 32,
-      borderRadius: 10,
-      marginRight: 6,
+      width: scale(32),
+      height: scale(32),
+      borderRadius: scale(10),
+      marginRight: scale(6),
       alignItems: "center",
       justifyContent: "center",
       borderWidth: 1,
@@ -76,14 +75,14 @@ export const createStatsStyles = (theme: any) =>
     },
 
     intervalMiss: {
-      borderColor: theme.shadowcolor,
+      borderColor: theme.border,
       backgroundColor: theme.card,
       opacity: 0.5,
     },
 
     intervalFail: {
-      borderColor: "#ff4a4a",
-      backgroundColor: "#ff4a4a33",
+      borderColor: theme.danger,
+      backgroundColor: theme.danger + "33",
     },
 
     intervalRestart: {
@@ -92,25 +91,24 @@ export const createStatsStyles = (theme: any) =>
     },
 
     intervalEmoji: {
-      fontSize: 18,
+      fontSize: fontScale(18),
       color: theme.text,
     },
-
 
     chartToggleRow: {
       flexDirection: "row",
       justifyContent: "center",
-      marginBottom: 12,
-      marginTop: 4,
+      marginBottom: verticalScale(12),
+      marginTop: verticalScale(4),
     },
 
     chartToggleButton: {
-      paddingVertical: 8,
-      paddingHorizontal: 18,
-      borderRadius: 8,
+      paddingVertical: verticalScale(8),
+      paddingHorizontal: scale(18),
+      borderRadius: scale(8),
       borderWidth: 1,
       borderColor: theme.primary + "66",
-      marginHorizontal: 6,
+      marginHorizontal: scale(6),
       backgroundColor: theme.card,
     },
 
@@ -123,23 +121,21 @@ export const createStatsStyles = (theme: any) =>
       fontWeight: "600",
     },
 
-
     pieContainer: {
       alignItems: "center",
       justifyContent: "center",
-      marginVertical: 10,
+      marginVertical: verticalScale(10),
     },
 
-
     barContainer: {
-      marginTop: 10,
-      marginBottom: 6,
+      marginTop: verticalScale(10),
+      marginBottom: verticalScale(6),
     },
 
     barTrack: {
-      height: 22,
-      borderRadius: 12,
-      backgroundColor: theme.background,
+      height: verticalScale(22),
+      borderRadius: scale(12),
+      backgroundColor: theme.input,
       flexDirection: "row",
       overflow: "hidden",
     },
@@ -151,21 +147,20 @@ export const createStatsStyles = (theme: any) =>
     barLabelsRow: {
       flexDirection: "row",
       justifyContent: "space-between",
-      marginTop: 6,
-      paddingHorizontal: 4,
+      marginTop: verticalScale(6),
+      paddingHorizontal: scale(4),
     },
 
     barLabel: {
-      color: theme.text,
-      opacity: 0.75,
-      fontSize: 13,
+      color: theme.textMuted,
+      fontSize: fontScale(13),
     },
 
     countRow: {
       flexDirection: "row",
       justifyContent: "space-between",
-      marginTop: 10,
-      marginBottom: 12,
+      marginTop: verticalScale(10),
+      marginBottom: verticalScale(12),
     },
 
     countItem: {
@@ -174,57 +169,55 @@ export const createStatsStyles = (theme: any) =>
     },
 
     legendDot: {
-      width: 10,
-      height: 10,
-      borderRadius: 5,
-      marginRight: 6,
+      width: scale(10),
+      height: scale(10),
+      borderRadius: scale(5),
+      marginRight: scale(6),
     },
 
     countText: {
-      color: theme.text,
-      opacity: 0.8,
-      fontSize: 14,
+      color: theme.textMuted,
+      fontSize: fontScale(14),
     },
 
     tasksHeader: {
       color: theme.text,
-      fontSize: 16,
+      fontSize: fontScale(16),
       fontWeight: "600",
-      marginBottom: 6,
-      marginTop: 6,
+      marginBottom: verticalScale(6),
+      marginTop: verticalScale(6),
     },
 
     tasksScrollContainer: {
-      maxHeight: 220,
-      paddingRight: 4,
+      maxHeight: verticalScale(220),
+      paddingRight: scale(4),
     },
 
     taskRow: {
       flexDirection: "row",
       alignItems: "flex-start",
-      paddingVertical: 8,
-      borderBottomWidth: 0.5,
-      borderColor: theme.shadowcolor + "55",
+      paddingVertical: verticalScale(8),
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      borderColor: theme.border,
     },
 
     taskDot: {
-      width: 12,
-      height: 12,
-      borderRadius: 6,
-      marginRight: 12,
-      marginTop: 4,
+      width: scale(12),
+      height: scale(12),
+      borderRadius: scale(6),
+      marginRight: scale(12),
+      marginTop: verticalScale(4),
     },
 
     taskTitle: {
       color: theme.text,
-      fontSize: 16,
+      fontSize: fontScale(16),
       fontWeight: "600",
-      marginBottom: 2,
+      marginBottom: verticalScale(2),
     },
 
     taskSub: {
-      color: theme.text,
-      opacity: 0.65,
-      fontSize: 13,
+      color: theme.textMuted,
+      fontSize: fontScale(13),
     },
   });
