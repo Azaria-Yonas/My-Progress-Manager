343505f6bcae7dfcf10a02c83bda4f000b45efbe
Azaria Yonas <azariayonas8@gmail.com>
2026-07-06 12:05:14 -0700

    Initial Commit

---
+++ Backend/agent/agent.py
@@ -0,0 +1,16 @@
+
+
+
+class agent:
+    def __init__(self):
+        pass
+
+    def message(self):
+        pass
+
+    def call_tools(self):
+        pass
+
+    def get_resources(self):
+        pass
+
