f53baf5fd8fa8136ca8625f23c9e7382a8823285
Azaria Yonas <azariayonas8@gmail.com>
2026-07-28 20:17:36 -0700

    Correctly handling JSONB data

--- Backend/routes/agent.py
+++ Backend/routes/agent.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request
 import psycopg as pg
+from psycopg.types.json import Jsonb
 from clients.psycopg_connect import psycopg_connect
 
 
@@ -34,14 +35,14 @@ def configure_agent(user_id, agent_name, avatar_picture, user_data):
                     curr.execute("""
                         INSERT INTO public.agent (user_id, agent_name, avatar_picture, user_data) VALUES (%s, %s, %s, %s)
                     """,
-                    (user_id, agent_name, avatar_picture, user_data))
+                    (user_id, agent_name, avatar_picture, Jsonb(user_data)))
                 else:
                     curr.execute("""
                         UPDATE public.agent
                         SET agent_name = %s, avatar_picture = %s, user_data = %s
                         WHERE user_id = %s
                     """,
-                    (agent_name, avatar_picture, user_data, user_id))
+                    (agent_name, avatar_picture, Jsonb(user_data), user_id))
 
                 return jsonify({"message": "Agent configured successfully"})
     except pg.Error as e:
@@ -57,7 +58,7 @@ def update_agent(user_id, agent_name, avatar_picture, user_data):
                     WHERE user_id = %s
                     RETURNING agent_name, avatar_picture, user_data
                 """,
-                (agent_name, avatar_picture, user_data, user_id))
+                (agent_name, avatar_picture, Jsonb(user_data), user_id))
 
                 row = curr.fetchone()
                 if row is None:
