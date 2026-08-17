8f643ffa360a95cda11973955d6b824460777972
Azaria Yonas <azariayonas8@gmail.com>
2026-08-16 19:42:40 -0700

    agentic loop

---
+++ Backend/agent/loop.py
@@ -0,0 +1,34 @@
+
+from Backend.agent.models.openai_connector import ChatModel, BaseModel, SummaryModel
+
+from message import Message
+
+from agent import Agent
+
+
+orchestrator = Agent(BaseModel)
+
+summarizer = Agent(SummaryModel)
+
+chat = Agent(ChatModel)
+BOOTSTRAP = "agent/agent_bootstrap/chat"
+
+
+
+
+def chat_loop(chat_history):
+
+    message = Message(bootstrap=BOOTSTRAP, history=chat_history)
+
+    return chat.ask(Message)
+
+
+    
+
+
+
+
+
+def orchestrator_loop():
+    print("orchestrator agent activated")
+    
