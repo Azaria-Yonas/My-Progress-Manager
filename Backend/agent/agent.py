4ca83d4b614775b39fac2fae805b36e7b5927239
Azaria Yonas <azariayonas8@gmail.com>
2026-08-17 17:05:23 -0700

    agent restructuring

--- Backend/agent/agent.py
+++ Backend/agent/agent.py
@@ -6,11 +6,18 @@ class Agent:
     def __init__(self, model):
         self._OpenAIModel = model
 
-    def ask(self, message):
+    def ask(self, message):   # this should belong to the model
+
+
         response = self._OpenAIModel.client.responses.create (
             model=self._OpenAIModel.model,
             tools=message.tools,
             input=message.chat_history
         )
         return response.output_text    
+
+
+
+
+
 
