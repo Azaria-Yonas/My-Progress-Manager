b232db0c06b1f5fb29531a879e0eefdd906ac999
Azaria Yonas <azariayonas8@gmail.com>
2026-08-20 15:04:26 -0700

    Removed input from instance attributes and instead appending it to chat history. Also changed default for the other instance attributes

--- Backend/agent/memory.py
+++ Backend/agent/memory.py
@@ -6,33 +6,31 @@ import json
 
 class Memory:
     def __init__(self, 
-        input = None,
-        bootstrap = None, 
-        history= None, 
-        tools = None, 
-        user_data= None,
+        bootstrap = "", 
+        history=  {}, 
+        tools = {}, 
+        user_data= {},
         **kwargs
     ):
         self.bootstrap = bootstrap
         self.chat_history = history
         self.tools = tools
         self.user_data = user_data
-        self.input = input
         self.additional_resources = kwargs
         
 
-        self._token_count = None
+        self.token_count = self.count_tokens() 
 
 
 
 
-    def token_count(self, model="gpt-5-nano"):
+    def count_tokens(self, model="gpt-5-nano"):
         """This function give a rough estimate of the number of tokens in memory"""
         text = str(self.user_data) + str(self.bootstrap) + str(self.tools) + str(self.chat_history) + str(self.additional_resources) 
         encoding = tiktoken.encoding_for_model(model)
 
-        self._token_count = len(encoding.encode(text))
-        return self._token_count
+        self.token_count = len(encoding.encode(text))
+        return self.token_count
 
 
 
