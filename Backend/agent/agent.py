41d65f683c36d399a500a867055c99d47a551427
Azaria Yonas <azariayonas8@gmail.com>
2026-08-21 19:46:32 -0700

    Updated Agent Class

--- Backend/agent/agent.py
+++ Backend/agent/agent.py
@@ -2,8 +2,8 @@ from pathlib import Path
 from routes.agent import get_agent
 import json
 
-from memory import Memory
-from models.openai_connector import OpenAIModel 
+from agent.memory import Memory
+from agent.models.openai_connector import OpenAIModel 
 
 
 
@@ -13,28 +13,31 @@ from models.openai_connector import OpenAIModel
 
 
 def load_bootstrap(path):
+    """This funciton recursively reads parses agent bootstap directories/files"""
     if not isinstance(path, Path):
         path = Path(path)
 
 
-    text = {}
+    bootstrap = {}
 
     if Path.is_file(path) and path.suffix == ".md":
         with open(path, 'r') as f:
-            text[path.name] = f.read() 
+            bootstrap[path.name] = f.read() 
 
     if Path.is_dir(path):
         files = list(path.rglob("*.md"))
         for i in files:
             with open(i, "r") as f:
-                text[path.name] = f.read() 
+                bootstrap[path.name] = f.read() 
 
-        return text
+        return bootstrap
 
     
 
 def load_tools (path):
-    path = Path(path)
+    "This function parses a json file that stores the tools "
+    if not isinstance(path, Path): 
+        path = Path(path)
     """Converts a json file into a python dictionary"""
     if Path.is_file(path) and Path.suffix == ".json":
         with open(path, "r") as f:
@@ -52,12 +55,14 @@ def load_user_data(uuid):
     if isinstance(response, tuple):
         response, status_code = response
 
-        if status_code in [200, 204, 206]: 
-            return None
+        if status_code in [200, 206]: 
 
-    data = response.get_json()
+            data = response.get_json()
 
-    return data[2]
+            return data[2]
+        
+
+    return "Use Default Configuration"
 
 
 
@@ -67,15 +72,44 @@ def load_user_data(uuid):
 
 class Agent:
     
-    def __init__(self, model= None, memory= None):
+    def __init__(self, model = OpenAIModel(), memory =  Memory()):
         self.model = model
         self.memory = memory
 
 
-    def ask(self, message):
-        if self.model:
-            return self.model.ask(message) 
-        return "Error"
+
+    def build_message(self, dump_all = False):
+        """This function construct a message to send.
+
+        Return dict()
+
+        Example:
+
+            message = {
+                "Bootstrap" : {},
+                "Chat History": {},
+                ...
+
+            
+            }
+        
+        
+        """
+
+        if dump_all is True:
+            return self.memory.dump()
+
+        return "Place Holder"
+    
+
+
+
+
+
+    # def ask(self, message):
+    #     if self.model:
+    #         return self.model.ask(message) 
+    #     return "Error"
 
 
 
@@ -87,11 +121,17 @@ class Agent:
 
 
 
-def create_agent(agent: Agent, memory: Memory, model: OpenAIModel, tools, bootstrap, user_data):
+def create_agent(agent: Agent, memory: Memory, model: OpenAIModel, tools = None, bootstrap = None, user_data = None):
 
-    memory.tools = tools
-    memory.bootstrap = bootstrap
-    memory.user_data = user_data
+    if tools is not None: 
+
+        memory.tools = tools
+
+    if bootstrap is not None:   
+        memory.bootstrap = bootstrap
+
+    if user_data is not None:
+        memory.user_data = user_data
 
 
     model = OpenAIModel("gpt-5-nano")
