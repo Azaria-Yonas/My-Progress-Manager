5651278d4e6ae0d9e996b16ef8acd71f8babe9f6
Azaria Yonas <azariayonas8@gmail.com>
2026-08-23 01:52:36 -0700

    Restructured Model

--- Backend/agent/models/openai_connector.py
+++ Backend/agent/models/openai_connector.py
@@ -1,27 +1,59 @@
 from openai import OpenAI
 from config import OPENAI_KEY
 from routes.agent import get_agent
+import json
 
+import requests
 
 
 
 
 class OpenAIModel:
-    def __init__(self, model):
-        self._client = OpenAI(api_key=OPENAI_KEY)
-        self._model = model
+    def __init__(self, model = "gpt-5-nano", max_output_tokens = 500, reasoning = None):
+        self.client = OpenAI(api_key=OPENAI_KEY)
+        self.model = model
+        self.reasoning = reasoning
+        self.output = max_output_tokens
 
-    def ask(self, message):  
+    def ask(self, memory, message):  
+
+        bootstrap = memory.get("Bootstrap")
+        chat_history = memory.get("Chat History")
+        user_data = memory.get("User Data")
+        additional_resources = memory.get("Additional Resources")
+
+        
+        instructions = {}
+        input = {}
+
+        if bootstrap is not None:
+            instructions["Bootsrap"] = bootstrap
+        if chat_history is not None: 
+
+            input["Chat_History"] = chat_history 
+
+        if user_data is not None:
+            instructions["User Data"] = user_data
+
+        if additional_resources is not None:
+            instructions["Additional Resources"] = additional_resources
+
+        input["Lastest request"]= message 
 
 
-        response = self._client.responses.create (
-            model=self._model,
-            # tools=message.tools,
-            # conversation=message.chat_history,
-            input=message
 
+
+
+
+        response = self.client.responses.create (
+            model=self.model,
+            instructions=json.dumps(instructions),
+            tools=memory.get("Tools"),
+            input= json.dumps(input)
 
         )
+
+
         return response   
 
 
@@ -31,68 +63,25 @@ class OpenAIModel:
 
 
 
-
-
-
+# Model Class with no SDK
 
 
 # class OpenAIModel:
-#     def __init__(self):
-#         self._client = OpenAI(api_key=OPENAI_KEY)
-#         self._model = None
+#     endpoint = "https://api.openai.com/v1/responses" 
+#     header = {
+#         "Authorization": f"Bearer {OPENAI_KEY}", 
+#         "Content-Type": "application/json",
+#     }
 
-#     @property
-#     def client(self):
-#         return self._client
-
-#     @property
-#     def model(self):
-#         return self._model
-
-#     def ask(self, message):   # this should belong to the model
-
-
-#         response = self._client.responses.create (
-#             model=self._model,
-#             tools=message.tools,
-#             input=message.chat_history
-#         )
-#         return response.output_text  
+#     def __init__(self, model, max_output_tokens = 500, reasoning = None):
+#         self.client = OpenAI(api_key=OPENAI_KEY)
+#         self.model = model
+#         self.reasoning = reasoning
+#         self.output = max_output_tokens 
 
 
 
 
-
-
-# class ChatModel(OpenAIModel):
-#     """This Model is Specialized in Handling certain tasks and primarily the Chat interface"""
-
-#     def __init__ (self):
-#         super().__init__()
-#         self._model = "gpt-5-nano"
-
-#     # def user_data(self, user_id):
-#     #     data = get_agent(user_id)
-#     #     return data[2]
-        
-        
-
-
-# class BaseModel(OpenAIModel): 
-#     """This is the primary model, capable of reasoning"""   
-
-#     def __init__ (self):
-#         super().__init__()
-#         self._model = "gpt-5-mini"
-
-
-
-# class SummaryModel(OpenAIModel):
-#     """The summary model exists exclusively to reduce costs"""
-
-#     def __init__ (self):
-#         super().__init__()
-#         self._model = "gpt-5-nano"
 
 
 
