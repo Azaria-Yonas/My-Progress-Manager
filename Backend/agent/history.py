d94b4c98fd3cee9cc3a6a63be2df5d60f19ab304
Azaria Yonas <azariayonas8@gmail.com>
2026-08-27 12:53:23 -0700

    Methods for adding data and fetching

--- Backend/agent/history.py
+++ Backend/agent/history.py
@@ -1,17 +1,15 @@
-import sqlite3
-from sqlalchemy import Column, create_engine, Integer, Table, DateTime, func
-from sqlalchemy import String, text, Uuid, Insert, Select, ForeignKey
-from sqlalchemy.dialects.sqlite import DATETIME, JSON
+from sqlalchemy import create_engine, DateTime, func
+from sqlalchemy import String, text, Uuid, Select, ForeignKey, true
+from sqlalchemy.dialects.sqlite import DATETIME, JSON, BOOLEAN
 from sqlalchemy.orm import DeclarativeBase, declarative_base, Mapped, mapped_column
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 
 from uuid import uuid4, UUID
 from datetime import datetime
 
-conn = sqlite3.connect("chat_history.db")
-
-
+engine =create_engine("sqlite:///history.db") 
+ 
 
 class ChatHistory(declarative_base()):
     """This function records the chat history between 
@@ -19,10 +17,55 @@ class ChatHistory(declarative_base()):
 
     __tablename__ = "chat_history"
 
+    conversation_id: Mapped[UUID] = mapped_column(Uuid, default=uuid4)
     message_id : Mapped[UUID] =  mapped_column(Uuid, primary_key=True, default=uuid4)
     user : Mapped[str] = mapped_column(String(10))
     message : Mapped[str] = mapped_column(String)
     date : Mapped[datetime] = mapped_column(DATETIME(timezone = True), server_default=func.now(), index=True)
+    status: Mapped[str] = mapped_column(String(20), server_default=text("'active'"), index=True)
+
+    
+
+
+    @classmethod
+    def append(cls, user, message):
+        with Session(engine) as session:
+            chat = cls(message= message, user = user)
+
+            session.add(chat)
+            session.commit()
+
+
+    @classmethod
+    def get_history(cls, active =False):
+        with Session(engine) as session:
+
+            if active == True:
+                stmt = Select(cls).where(cls.status == 'active')
+
+                
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def __repr__(self):
+        return f"{self.user} :  {self.message}"
 
 
 
@@ -39,8 +82,6 @@ class AgentHistory(declarative_base()):
     date: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now(), index=True)
     output: Mapped[dict] = mapped_column(JSON)
     message_id : Mapped[UUID] = mapped_column(Uuid, ForeignKey = ForeignKey("chat_history.message_id"))
-
-
 
 
 
